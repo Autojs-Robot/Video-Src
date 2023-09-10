350645b158984d356fd95bcd28a80277157776de
ranxfan <ranxfan@gmail.com>
2023-09-11 02:24:56 +0800

    feat:更新下载文件

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,21 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/:time/:folder', function (req, res, next) {
-  const { time, folder } = req.params;
-  let t = time === 'yesterday' ? moment().subtract(1, 'days') : moment(time);
-  if (!t.isValid()) {
-    if (time == 'today') {
-      t = moment()
-    } else {
-      t = moment().subtract(1, 'days')
+
+router.get('/data/:time/:folder/:name', function (req, res, next) {
+  const from = req.path
+  console.log("下载文件", req.path);
+  res.sendFile(from, (err) => {
+    if (!err) {
+      const name = from.toString().replace(/\//g, '-')
+      const to = path.join("/data", "used", name)
+      fs.rename(from, to, (err) => {
+        console.log(from, "=>", to, err);
+      })
     }
-  }
-  const dir = path.join('/upload', t.format('YYYY-MM-DD'), folder)
-  res.render('index', { title: 'Express', file: dir });
+  });
 });
+
 
 router.get('/upload/:time/:folder', function (req, res, next) {
   const { time, folder } = req.params;
@@ -40,16 +42,32 @@ router.get('/upload/:time/:folder', function (req, res, next) {
   const dir = path.join("/data", t.format('YYYY-MM-DD'), folder)
   const filename = getFile(dir);
   const from = path.resolve(dir, filename)
-  res.sendFile(from, (err) => {
-    if (!err) {
-      const name = from.toString().replace(/\//g, '-')
-      const to = path.join("/data", "used", name)
-      fs.rename(from, to, (err) => {
-        console.log(from, "=>", to, err);
-      })
-    }
-  });
+  res.redirect(from.toString())
+  // res.sendFile(from, (err) => {
+  //   if (!err) {
+  //     const name = from.toString().replace(/\//g, '-')
+  //     const to = path.join("/data", "used", name)
+  //     fs.rename(from, to, (err) => {
+  //       console.log(from, "=>", to, err);
+  //     })
+  //   }
+  // });
 
+});
+
+
+router.get('/:time/:folder', function (req, res, next) {
+  const { time, folder } = req.params;
+  let t = time === 'yesterday' ? moment().subtract(1, 'days') : moment(time);
+  if (!t.isValid()) {
+    if (time == 'today') {
+      t = moment()
+    } else {
+      t = moment().subtract(1, 'days')
+    }
+  }
+  const dir = path.join('/upload', t.format('YYYY-MM-DD'), folder)
+  res.render('index', { title: 'Express', file: dir });
 });
 
 module.exports = router;
